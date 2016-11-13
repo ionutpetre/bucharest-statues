1a66e75d1d015128becf6cacdbf3db4b9507365e
Mihai Alexandru Iordache <mihai.iordache@misys.com>
2016-11-13 10:57:38 +0200

    - $mbDialog for welcome msj - $mdTabs - 3 context buttons [wip]

--- public/scripts/statues.controller.js
+++ public/scripts/statues.controller.js
@@ -1,3 +1,41 @@
-angular.module('StatuesApp').controller('StatuesController', ['$scope', 'statues', function($scope, statues) {
+angular.module('StatuesApp').controller('StatuesController', ['$scope', 'statues','$mdSidenav','$timeout', '$mdDialog',
+    function($scope, statues, $mdSidenav, $timeout, $mdDialog) {
+
     $scope.statues = statues.data;
+    $scope.cotinue = false;
+    $scope.tabs    = [
+        {
+            "tabId": 1,
+            "tabName": "Toate statuile"
+
+        }, 
+        {
+            "tabId": 2,
+            "tabName": "Statui in ordine alfabetica"
+        },
+        {
+            "tabId": 3,
+            "tabName": "Statui dupa profile"
+        }
+    ];
+
+
+    function showConfirm(ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    var confirm = $mdDialog.confirm()
+          .title('Aplicatia STATUI IN BUCURESTI este realizata  in anul' +
+            ' universitar 2016-2017 de catre studentii de la masterul de IE Facultatea CSIE din ASE.')
+          .textContent('')
+          .ariaLabel('Lucky day')
+          .targetEvent(ev)
+          .ok('Continuare');
+
+    $mdDialog.show(confirm).then(function() {
+      $scope.continue = true;
+    }, function() {
+      //
+    });
+  };
+
+      showConfirm();  
 }]);
