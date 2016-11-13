3c58fc4c99aed8fb604c58c8182f4559cb79ae81
Ionut Petre <ionut.petre@ro.ibm.com>
2016-11-13 14:59:20 +0200

    Added filter functionality

--- public/scripts/app.constants.js
+++ public/scripts/app.constants.js
@@ -4,13 +4,17 @@ angular.module('StatuesApp')
         "CONTENT": "Aplicatia STATUI IN BUCURESTI este realizata  in anul universitar 2016-2017 de catre studentii de la masterul de IE Facultatea CSIE din ASE.",
         "OK_BTN": "Continuare"
     })
-    .constant("TABS", [{
+    .constant("FILTERS", [{
         "id": 1,
-        "name": "Toate statuile"
+        "name": "Toate statuile",
+        "selected": true
     }, {
         "id": 2,
-        "name": "Statui in ordine alfabetica"
+        "name": "Statui in ordine alfabetica",
+        "selected": false
     }, {
         "id": 3,
-        "name": "Statui dupa profile"
-    }]);
+        "name": "Statui dupa categorie",
+        "selected": false
+    }])
+    .constant("CATEGORIES", ['Scriitori', 'Actori', 'Domnitori', 'Ostasi', 'Savanti']);
--- public/scripts/statues.controller.js
+++ public/scripts/statues.controller.js
@@ -1,8 +1,29 @@
-angular.module('StatuesApp').controller('StatuesController', ['$scope', 'statues', 'DIALOG', 'TABS', '$mdSidenav', '$mdDialog', '$cookies',
-    function($scope, statues, DIALOG, TABS, $mdSidenav, $mdDialog, $cookies) {
+angular.module('StatuesApp').controller('StatuesController', ['$scope', 'statues', 'DIALOG', 'FILTERS', 'CATEGORIES', '$mdSidenav', '$mdDialog', '$cookies',
+    function($scope, statues, DIALOG, FILTERS, CATEGORIES, $mdSidenav, $mdDialog, $cookies) {
         $scope.statues = statues.data;
-        $scope.tabs = TABS;
+        $scope.filters = FILTERS;
+        $scope.categories = CATEGORIES;
+        $scope.setSelectedFilters = setSelectedFilters;
         showConfirm();
+        setFilterValues({}, '');
+
+        function setFilterValues(search, orderBy) {
+            $scope.search = search;
+            $scope.orderBy = orderBy;
+        }
+
+        function setSelectedFilters(filterIdx) {
+            angular.forEach($scope.filters, function(filter, filterKey) {
+                filter.selected = (filterIdx == filterKey);
+                if (filter.selected) {
+                    switch (filter.id) {
+                        case $scope.filters[0].id: setFilterValues({}, ''); break;
+                        case $scope.filters[1].id: setFilterValues({}, 'name'); break;
+                        case $scope.filters[2].id: setFilterValues({}, 'category'); break;
+                    }
+                }
+            });
+        }
 
         function getExpireDate() {
             var expireDate = new Date();
