f543398427e1d409878eeb93722377d8464fdfe9
Mihai Alexandru Iordache <mihai.iordache@misys.com>
2016-11-30 13:04:39 +0200

    - changed grid layout - added return button in html wip

--- public/scripts/statue-card.controller.js
+++ public/scripts/statue-card.controller.js
@@ -1,3 +1,8 @@
-angular.module('StatuesApp').controller('StatueCardController', ['$scope', function($scope) {
-    
+angular.module('StatuesApp').controller('StatueCardController', ['$scope','$location', function($scope, $location) {
+
+    $scope.goHome = goHome;
+
+    function goHome(path) {
+        $location.path(path);
+    };
 }]);
