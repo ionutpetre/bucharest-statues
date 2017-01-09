5dc06a960c8d5c6ffcd4cfe0907feb0a9b0b0f23
Andrei Deleanu <AndreiDeleanu@ro.ibm.com>
2017-01-09 16:44:24 +0200

    I implemented the statue details page.

--- public/scripts/statue.controller.js
+++ public/scripts/statue.controller.js
@@ -1,3 +1,38 @@
 angular.module('StatuesApp').controller('StatueController', ['$scope', 'statue', function($scope, statue) {
     $scope.statue = statue.data;
+    $scope.myInterval=3000;
+    $scope.noWrapSlides = false;
+    $scope.active = 1;
+    $scope.slides=[
+                   {
+                	   image:"images/statues/statue_"+$scope.statue.id+"/front.jpg"
+                   },
+                   
+                   {
+                	   image:"images/statues/statue_"+$scope.statue.id+"/left.jpg"
+                   },
+                   
+                   {
+                	   image:"images/statues/statue_"+$scope.statue.id+"/right.jpg"
+                   },
+                   
+                   {
+                	   image:"images/statues/statue_"+$scope.statue.id+"/back.jpg"
+                   }
+                   ];
+    
+    $scope.currentIndex = 0;
+    $scope.setCurrentSlideIndex = function (index) {
+        $scope.currentIndex = index;
+    };
+    $scope.isCurrentSlideIndex = function (index) {
+        return $scope.currentIndex === index;
+    };
+    
+    $scope.prevSlide = function () {
+        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+    };
+    $scope.nextSlide = function () {
+        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+    };
 }]);
